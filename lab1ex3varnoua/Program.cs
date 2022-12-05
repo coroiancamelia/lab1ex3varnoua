1dcd6089300b18836476bcd2603dc975a301c86c
coroiancamelia <coroiancamelia@outlook.com>
2022-12-05 22:32:51 +0200

    Add project files.

---
+++ lab1ex3varnoua/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab1ex3varnoua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          /* Ex3
+          Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+          tastaura */
+
+          
+
+            Console.WriteLine("Acest program va afisa ultima cifra a unui numar intreg");
+            Console.WriteLine("Introduceti numarul intreg");
+
+            int numarulIntreg = int.Parse(Console.ReadLine());
+         
+            Console.WriteLine("Ultima cifra este:" + numarulIntreg % 10);
+            
+        }
+    }
+}
